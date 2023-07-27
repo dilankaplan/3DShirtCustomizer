5676906f1ca0d5a120bfa9c049ecac02720df5fc
Dilan Kaplan <45143995+dilankaplan@users.noreply.github.com>
2023-07-27 16:47:11 +0300

    shirt customizer

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import ColorPicker from "./ColorPicker";
+
+export {
+    CustomButton,
+    ColorPicker,
+    FilePicker,
+    Tab,
+    AIPicker,
+}
